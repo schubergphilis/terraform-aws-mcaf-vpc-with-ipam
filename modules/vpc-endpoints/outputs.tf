578d556c721fedbd613fdbf699b542bb59777c7a
Marwin Baumann <mbaumann@schubergphilis.com>
2025-02-25 12:58:16 +0100

    remove faulty outputs

--- modules/vpc-endpoints/outputs.tf
+++ modules/vpc-endpoints/outputs.tf
@@ -3,16 +3,6 @@ output "endpoints" {
   value       = aws_vpc_endpoint.default
 }
 
-output "endpoint_ids" {
-  description = "The list of IDs of the VPC endpoints created."
-  value       = aws_vpc_endpoint.default[*].id
-}
-
-output "endpoint_arns" {
-  description = "The list of Amazon Resource Names (ARNs) of the VPC endpoints."
-  value       = aws_vpc_endpoint.default[*].arn
-}
-
 output "custom_route53_zones" {
   description = "A map of zone_id and zone_name for each custom DNS zone created, indexed by the endpoint key."
   value = {
