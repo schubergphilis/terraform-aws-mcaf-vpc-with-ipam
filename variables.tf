10eb6d75a8160284791ba10916578889b42d7c24
mptier <62100541+marlonparmentier@users.noreply.github.com>
2025-03-19 15:33:48 +0100

    Revert "Update comparison to require either log_destination or bucket_name if flow logs configuration is given"
    This reverts commit 6d0f318038baa0f6979a553af7032c11f764b9c9.

--- variables.tf
+++ variables.tf
@@ -8,6 +8,8 @@ variable "aws_vpc_ipam_pool" {
   description = "ID of the IPAM pool to get CIDRs from."
 }
 
+# s3_flow_logs_configuration.log_destination accepts full S3 ARNs, optionally including keys. Example:
+# "s3://{bucket_name}/{key_name}" will create a folder in the S3 bucket with the {key_name}
 variable "s3_flow_logs_configuration" {
   type = object({
     bucket_name              = optional(string)
@@ -28,7 +30,7 @@ variable "s3_flow_logs_configuration" {
   description = "Variables to enable S3 flow logs for the VPC. Use 'bucket_name' to log to an S3 bucket created by this module. Alternatively, use 'log_destination' to specify a self-managed S3 bucket. The 'log_destination' variable accepts full S3 ARNs, optionally including object keys."
 
   validation {
-    condition     = var.s3_flow_logs_configuration == null || (var.s3_flow_logs_configuration.log_destination == null && var.s3_flow_logs_configuration.bucket_name != null) || (var.s3_flow_logs_configuration.log_destination != null && var.s3_flow_logs_configuration.bucket_name == null)
+    condition     = var.s3_flow_logs_configuration == null || (try(var.s3_flow_logs_configuration.log_destination, null) != null || try(var.s3_flow_logs_configuration.bucket_name, null) != null)
     error_message = "Either log_destination or bucket_name must be specified in s3_flow_logs_configuration if the configuration is provided."
   }
 }
